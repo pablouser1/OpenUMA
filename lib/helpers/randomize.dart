78d36ab69edecdb97c2e80356828dde04b57aff8
Pablo Ferreiro <me@pabloferreiro.es>
2024-04-08 18:26:32 +0200

    upgrading deps

--- lib/helpers/randomize.dart
+++ lib/helpers/randomize.dart
@@ -3,15 +3,13 @@ import 'dart:math';
 import 'package:openuma/models/coords.dart';
 
 class Randomize {
-  static const _chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+  static const _chars =
+      'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
   static final Random _rnd = Random();
 
   static String string([int length = 32]) {
-    return String.fromCharCodes(
-      Iterable.generate(
-        length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))
-      )
-    );
+    return String.fromCharCodes(Iterable.generate(
+        length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
   }
 
   /// Devuelve posición aleatoria dentro del area max
